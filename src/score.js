cac9aacb6e6cdf8d8d38725bef549b80772fde41
Piotr Kaminski <piotr@ideanest.com>
2018-12-14 16:05:16 -0800

    Show last bar in full score, and allow drawing on top of position marker.

--- src/score.js
+++ src/score.js
@@ -111,7 +111,7 @@ class Score {
     if (this._pages) scoreElement.removeChild(this._pages[this._currentPage].element);
     this._pages = [];
     let barIndex = 0;
-    while (barIndex < this._analysis.bars.length - 1) {
+    while (barIndex < this._analysis.bars.length) {
       const page = this._paintPage(barIndex, barIndex ? 0 : HEADER_HEIGHT);
       if (barIndex === 0) {
         page.element = this._buildHeader(page.element);
@@ -154,11 +154,9 @@ class Score {
     element.appendChild(canvas);
     const numStaffs = Math.floor(height / ((this._staffHeight + STAFF_GAP) * scale)) || 1;
     let top = 0;
-    for (let i = 0; i < numStaffs; i++) {
-      if (barIndex >= bars.length - 1) break;
+    for (let i = 0; i < numStaffs && barIndex < bars.length; i++) {
       const firstBarIndex = barIndex;
       const left = firstBarIndex ? bars[firstBarIndex].leftEdge : this._analysis.leftEdge;
-      barIndex++;
       while (
         barIndex + 1 < bars.length &&
         (bars[barIndex + 1].rightEdge - left) * this._imageRatio * scale < width
